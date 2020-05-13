2b811a4e078429cf907a071e159432aac9e5a284
darrenwshort <dwshort707@gmail.com>
2020-05-13 16:08:45 -0700

    fixed comments

--- findresource.php
+++ findresource.php
@@ -1,16 +1,26 @@
 
-  
+
 <?php
+// ----------------------------------
+// ViT 4.20.20 Galvanize Course      
+// Student:       Darren Short
+// Assignment:    Final Project
+// File:          findresource.php
+// ----------------------------------
+
+// check if searchterm key passed from ajax call
 if(isset($_GET['searchterm']))
 {
   $str = $_GET['searchterm'];
-  // text file contains excerpt from CRC catalog
+
+  // json contains objects of 1) url & 2) array of keywords
   $file = 'suggestions.json';
 
   // read json into array.
   $json = file_get_contents($file);
   $json_data = json_decode($json, true);
 
+  // return ordered list of hyperlinks
   echo "<ol>";
   foreach($json_data as $data){
     if(in_array($str, $data["keywords"])){
