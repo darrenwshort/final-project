bd7fcb32a20fc8b6670981f5b59a32cf5a679512
darrenwshort <dwshort707@gmail.com>
2020-05-12 17:54:46 -0700

    uh, added code? :-)

---
+++ suggestions.js
@@ -0,0 +1,28 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    // func to search for resource
+    function findResource() {
+        let search = document.getElementById("searchterm");
+        let str = search.value;
+        if (str.length == 0) {
+          document.getElementById("results-div").innerHTML = "";
+          return;
+        } else {
+          var xmlhttp = new XMLHttpRequest();
+          xmlhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+              document.getElementById("results-div").innerHTML = this.responseText;
+            }
+          };
+          xmlhttp.open("GET", "getresource.php?r=" + str, true);
+          xmlhttp.send();
+        }
+    }
+
+    // on each 'key up' stroke, kick off search via findResource().
+    document.addEventListener("keyup", () => {
+        findResource();
+    });
+
+});
