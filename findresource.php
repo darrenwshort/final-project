a6dcf73446c2eec31ee17b74a1a682185e1cb24c
darrenwshort <dwshort707@gmail.com>
2020-05-14 15:13:45 -0700

    .

--- findresource.php
+++ findresource.php
@@ -20,19 +20,38 @@ if(isset($_GET['searchterm']))
   $json = file_get_contents($file);
   $json_data = json_decode($json, true);
 
-  // prep regex for efficient match; case-insensitive
+    // prep regex for efficient match; case-insensitive
   $regex = '/' . $str . '/i';
 
-  // return ordered list of hyperlinks
+  // open ordered list of hyperlinks.
   echo "<ol>";
-  foreach($json_data as $json_obj){
-    foreach($json_obj["keywords"] as $keyword){
-      if(preg_match($regex, $keyword)){
-        echo "<li><a class='newlinks' target='_blank' href='" . $json_obj["url"] . "'>" . $json_obj["url"] . '</a>(<span class="italicize">' . $json_obj["keywords"][0] . '</span>)</li>';
-        break;
+
+  // if search is 'all', display everything.
+  if(preg_match($regex, 'all')){
+    foreach($json_data as $obj)
+    echo "<li>" . 
+          "<a class='newlinks' target='_blank' href='" . 
+          $obj["url"] . "'>" . $obj["url"] . 
+          "</a>(<span class='italicize'>" . 
+          $obj["keywords"][0] . 
+          "</span>)</li>";
+  } else {
+      // display relevant content based on search string.
+      foreach($json_data as $json_obj){
+        foreach($json_obj["keywords"] as $keyword){
+          if(preg_match($regex, $keyword)){
+            echo "<li>" . 
+                  "<a class='newlinks' target='_blank' href='" . 
+                  $json_obj["url"] . "'>" . $json_obj["url"] . 
+                  "</a>(<span class='italicize'>" . 
+                  $json_obj["keywords"][0] . 
+                  "</span>)</li>";
+            break;
+          }
+        }
       }
-    }
   }
+  // close ordered list.
   echo "</ol>";
 }
 else { echo "Nothing bro!";}
